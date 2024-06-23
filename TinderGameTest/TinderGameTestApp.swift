3aa8f3ab9cf91800445816841b74de0f5832e2af
Bing <6bingj@gmail.com>
2024-06-23 14:31:00 -0700

    Initial Commit

---
+++ TinderGameTest/TinderGameTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderGameTestApp.swift
+//  TinderGameTest
+//
+//  Created by Bingjian Liu on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderGameTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
