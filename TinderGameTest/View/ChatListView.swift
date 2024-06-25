bea31e3ac96377b666972f5029ad9f112d822dca
Bing <6bingj@gmail.com>
2024-06-25 07:29:24 -0700

    UI improvement

---
+++ TinderGameTest/View/ChatListView.swift
@@ -0,0 +1,50 @@
+//
+//  ChatListView.swift
+//  TinderGameTest
+//
+//  Created by Bingjian Liu on 6/25/24.
+//
+
+import SwiftUI
+
+struct ChatListView: View {
+    var body: some View {
+        
+        let realChat =  Chat(name: "Ryan", lastMessage: "Recently active, match now!", imageName: "profile1", isVerified: true, gameInvite: true)
+        
+        VStack {
+            ZStack {
+                HStack {
+                    Spacer()
+                    
+                    Image("Shield")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 30)
+                        .padding(10)
+                }
+                
+                Image("TinderLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 60)
+            }
+            
+            List {
+                Text("Messages")
+                    .font(.headline)
+                    .padding(.vertical,5)
+                    .padding(.horizontal,3)
+                NavigationLink(destination: ChatView()) {
+                    ChatRowView(chat: realChat)
+                }
+                
+            }
+            .listStyle(.plain)
+        }
+    }
+}
+
+#Preview {
+    ChatListView()
+}
---
+++ TinderGameTest/View/PersonalTabView.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalTabView.swift
+//  TinderGameTest
+//
+//  Created by Bingjian Liu on 6/25/24.
+//
+
+import SwiftUI
+
+struct PersonalTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonalTabView()
+}
